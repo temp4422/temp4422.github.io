3e0133561b30ecd93112f1734db7ebda47388140
user <user@MacBook-Pro-user.local>
2023-10-21 08:59:17 +0300

    WIP

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -1,20 +1,35 @@
 export default function typewriter() {
-  // Create elements
-  const container = document.createElement('div')
-  container.setAttribute('id', 'typewriter-container')
-  const text = document.createElement('p')
-  text.setAttribute('id', 'typewriter')
-  text.setAttribute('data-wait', 1000)
-  text.setAttribute(
-    'data-words',
-    "['Вітання!', 'Greetings!', 'Les salutations!', 'Grüße!', 'Saluti!', '问候！', 'Приветствие!', '!سلام تحية', 'Saluto!', 'Salutació!', '!דְרִישַׁת שָׁלוֹם', 'Salom!', '¡Saludo!', 'ຊົມເຊີຍ!', '挨拶！', 'გამარჯობა!', 'Powitanie!', 'Beannacht!', 'Hilsen!', 'Hälsning!', 'Мэндчилгээ!', 'Pozdrav!', 'Üdvözlet!', 'Salut!', 'Sveikinu!', 'Salam!', 'Salutem!', 'تبریک!', 'Saudações!', 'Lep pozdrav!', 'S pozdravom!', 'Kwaziso!', 'Salaan!', 'Χαιρετισμός!', 'Salamlayıram!', 'Поздрав!', 'Aloha!', 'शुभकामना!', 'Kveðja!', 'Прывітанне!']"
-  )
+  console.log('OK')
+  // // Create elements
+  // const container = document.createElement('div')
+  // container.setAttribute('id', 'typewriter-container')
+  // const text = document.createElement('p')
+  // text.setAttribute('id', 'typewriter')
+  // text.setAttribute('data-wait', 1000)
+  // text.setAttribute(
+  //   'data-words',
+  //   "['Вітання!', 'Greetings!', 'Les salutations!', 'Grüße!', 'Saluti!', '问候！', 'Приветствие!', '!سلام تحية', 'Saluto!', 'Salutació!', '!דְרִישַׁת שָׁלוֹם', 'Salom!', '¡Saludo!', 'ຊົມເຊີຍ!', '挨拶！', 'გამარჯობა!', 'Powitanie!', 'Beannacht!', 'Hilsen!', 'Hälsning!', 'Мэндчилгээ!', 'Pozdrav!', 'Üdvözlet!', 'Salut!', 'Sveikinu!', 'Salam!', 'Salutem!', 'تبریک!', 'Saudações!', 'Lep pozdrav!', 'S pozdravom!', 'Kwaziso!', 'Salaan!', 'Χαιρετισμός!', 'Salamlayıram!', 'Поздрав!', 'Aloha!', 'शुभकामना!', 'Kveðja!', 'Прывітанне!']"
+  // )
+  // container.append(text)
 
-  container.append(text)
-
-  // Append container to root
+  const typewriter = `
+  <div id="typewriter-container" style="width: max-content; height: max-content; margin: 10rem auto; font-size: 2rem;">
+    <p
+      id="typewriter"
+      data-wait="1000"
+      data-words="['Вітання!', 'Greetings!', 'Les salutations!', 'Grüße!', 'Saluti!', '问候！', 'Приветствие!', '!سلام تحية', 'Saluto!', 'Salutació!', '!דְרִישַׁת שָׁלוֹם', 'Salom!', '¡Saludo!', 'ຊົມເຊີຍ!', '挨拶！', 'გამარჯობა!', 'Powitanie!', 'Beannacht!', 'Hilsen!', 'Hälsning!', 'Мэндчилгээ!', 'Pozdrav!', 'Üdvözlet!', 'Salut!', 'Sveikinu!', 'Salam!', 'Salutem!', 'تبریک!', 'Saudações!', 'Lep pozdrav!', 'S pozdravom!', 'Kwaziso!', 'Salaan!', 'Χαιρετισμός!', 'Salamlayıram!', 'Поздрав!', 'Aloha!', 'शुभकामना!', 'Kveðja!', 'Прывітанне!']"
+    >
+      <span id="typewriter"></span>
+    </p>
+    <style id="cursor" type="text/css">
+      #typewriter > span {border-right: 0.05em solid; animation: caret 1s steps(1) infinite;} @keyframes caret {50% {border-color: transparent;}}
+    </style>
+  </div>
+  `
+  // Append typewriter-container to root
   const root = document.querySelector('#root')
-  root.appendChild(container)
+  // root.append(typewriter)
+  root.insertAdjacentHTML('beforeend', typewriter)
 
   /****************************** Typewriter ******************************/
   class TypeWriter {
@@ -44,7 +59,8 @@ export default function typewriter() {
       }
 
       // Insert txt into element
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+      this.txtElement.innerHTML = `<span>${this.txt}</span>`
+      // this.txtElement.innerHTML = `<span style="border-right: 0.05em solid; animation: caret 1s steps(1) infinite; @keyframes caret {50% {border-color: transparent;}}">${this.txt}</span>` // NOT WORK!
 
       // Initial Type Speed
       let typeSpeed = 200
@@ -64,7 +80,7 @@ export default function typewriter() {
         // Move to next word
         this.wordIndex++
         // Pause before start typing
-        typeSpeed = 500
+        typeSpeed = this.wait
       }
       setTimeout(() => this.type(), typeSpeed)
     }
@@ -75,12 +91,14 @@ export default function typewriter() {
     const preWords = txtElement.getAttribute('data-words').replace(/'/gi, '"')
     const words = JSON.parse(preWords)
     const wait = txtElement.getAttribute('data-wait')
+
     // Add cursor
-    const cursor = document.createElement('style')
-    cursor.type = 'text/css'
-    cursor.innerHTML =
-      '.typewriter > .txt {border-right: 0.05em solid; animation: caret 1s steps(1) infinite;} @keyframes caret {50% {border-color: transparent;}} '
-    document.body.appendChild(cursor)
+    // const cursor = document.createElement('style')
+    // cursor.type = 'text/css'
+    // cursor.innerHTML =
+    //   '#typewriter > .txt {border-right: 0.05em solid; animation: caret 1s steps(1) infinite;} @keyframes caret {50% {border-color: transparent;}} '
+    // document.body.appendChild(cursor)
+
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait)
   }
