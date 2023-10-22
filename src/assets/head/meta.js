d9bdb7c8443e90d7b912cadf22dbc34092252f73
user <user@MacBook-Pro-user.local>
2023-10-23 00:36:36 +0300

    Add favicon.ico

--- src/assets/head/meta.js
+++ src/assets/head/meta.js
@@ -1,5 +1,5 @@
 const headMeta = `
     <meta name="description" content="My portfolio website">
-
+    <link rel="icon" type="image/x-icon" href="/src/assets/head/favicon.ico">
   `
 document.head.innerHTML += headMeta
