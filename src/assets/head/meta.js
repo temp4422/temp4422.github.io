7ae576c4106757901ce943508ad519e3af73c1b6
user <user@MacBook-Pro-user.local>
2023-10-23 00:41:06 +0300

    Refactor head meta

--- src/assets/head/meta.js
+++ src/assets/head/meta.js
@@ -1,4 +1,6 @@
 const headMeta = `
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="My portfolio website">
     <link rel="icon" type="image/x-icon" href="/src/assets/head/favicon.ico">
   `
