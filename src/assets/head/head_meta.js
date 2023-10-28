f92ea1d5ae33b99f306f4a04b5ca737f3d817945
user <user@MacBook-Pro-user.local>
2023-10-28 20:00:20 +0300

    WIP GTM

--- src/assets/head/head_meta.js
+++ src/assets/head/head_meta.js
@@ -8,7 +8,7 @@ const headMeta = `
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MWDRL72');</script>
+    })(window,document,'script','dataLayer','GTM-PRTMM7JX');</script>
     <!-- End Google Tag Manager -->
   `
 document.head.insertAdjacentHTML('beforeend', headMeta)
