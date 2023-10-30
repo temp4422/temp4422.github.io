b0a52c628a79177cd3e0b1854d0b62d8e344f2c7
user <user@MacBook-Pro-user.local>
2023-10-30 21:44:02 +0200

    Fix navmenu staggering

--- index.js
+++ index.js
@@ -52,32 +52,32 @@ htmlFiles.forEach((fileName) => {
 console.log(`Tags replaced in all HTML pages and saved to the ${dist} directory. 👍 \n`)
 
 /* ************************************************************************************** */
-// Start optimization
-console.log(`Start optimization all pages in ${dist} 🔨`)
+// // Start optimization
+// console.log(`Start optimization all pages in ${dist} 🔨`)
 
-// Optimize HTML in a given file
-function optimizeHtmlFile(filePath) {
-  let htmlContent = fs.readFileSync(filePath, 'utf-8')
-  const minifiedHtml = minify(htmlContent, {
-    collapseWhitespace: true,
-    removeComments: true,
-    minifyJS: true,
-    minifyCSS: true,
-  })
-  fs.writeFileSync(filePath, minifiedHtml, 'utf-8')
-}
+// // Optimize HTML in a given file
+// function optimizeHtmlFile(filePath) {
+//   let htmlContent = fs.readFileSync(filePath, 'utf-8')
+//   const minifiedHtml = minify(htmlContent, {
+//     collapseWhitespace: true,
+//     removeComments: true,
+//     minifyJS: true,
+//     minifyCSS: true,
+//   })
+//   fs.writeFileSync(filePath, minifiedHtml, 'utf-8')
+// }
 
-// Get a list of all HTML files in the dist directory
-const distFiles = fs.readdirSync(dist).filter((file) => path.extname(file) === '.html')
+// // Get a list of all HTML files in the dist directory
+// const distFiles = fs.readdirSync(dist).filter((file) => path.extname(file) === '.html')
 
-// Process each HTML file in the dist directory
-distFiles.forEach((fileName) => {
-  const distFilePath = path.join(dist, fileName)
-  optimizeHtmlFile(distFilePath)
-  console.log(`Optimized ${fileName} in the dist directory.`)
-})
+// // Process each HTML file in the dist directory
+// distFiles.forEach((fileName) => {
+//   const distFilePath = path.join(dist, fileName)
+//   optimizeHtmlFile(distFilePath)
+//   console.log(`Optimized ${fileName} in the dist directory.`)
+// })
 
-console.log(`HTML optimization completed for all HTML files in the ${dist} directory. 👍 \n`)
+// console.log(`HTML optimization completed for all HTML files in the ${dist} directory. 👍 \n`)
 
 /* ************************************************************************************** */
 // Start optimization of images
