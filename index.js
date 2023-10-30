2839b323ffcd2184c498de0472794776023b9de9
user <user@MacBook-Pro-user.local>
2023-10-30 18:49:20 +0200

    WIP

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const minify = require('html-minifier').minify
 const sharp = require('sharp')
 
 // Define source and destination directories, file extension, and tag files directory
-const pagesDir = './src/pages/'
-const componentsDir = './src/components/'
-const assetsDir = './src/assets/img/jpg'
-const distDir = './docs/'
-const distAssetsDir = './docs/assets/img/'
-const htmlFileExtension = '.html'
+const srcPages = './src/pages/'
+const srcComponents = './src/components/'
+const srcAssets = './src/assets/'
+const srcAssetsImg = './src/assets/img/jpg/'
+const dist = './docs/'
+const distAssets = './docs/assets/'
+const distAssetsImg = './docs/assets/img/'
 
 // Create dir if it doesnt exists
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir, { recursive: true })
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist, { recursive: true })
 }
 
 // Start replacing
-console.log(`Start replacing corresponding tags for all pages in ${pagesDir} 🔨 \n`)
+console.log(`Start replacing corresponding tags for all pages in ${srcPages} 🔨 \n`)
 
 // Read and replace tags in a given HTML file
 function replaceTagsInFile(srcPath, distPath) {
@@ -28,8 +29,8 @@ function replaceTagsInFile(srcPath, distPath) {
   if (matches) {
     matches.forEach((match) => {
       const tagName = match.match(/\w+-x/)[0]
-      const fileName = `${tagName}${htmlFileExtension}`
-      const filePath = path.join(componentsDir, fileName)
+      const fileName = `${tagName}.html`
+      const filePath = path.join(srcComponents, fileName)
       const fileContent = fs.readFileSync(filePath, 'utf-8')
       htmlContent = htmlContent.replace(match, fileContent)
     })
@@ -38,67 +39,65 @@ function replaceTagsInFile(srcPath, distPath) {
 }
 
 // Get a list of all HTML files in the source directory
-const htmlFiles = fs
-  .readdirSync(pagesDir)
-  .filter((file) => path.extname(file) === htmlFileExtension)
+const htmlFiles = fs.readdirSync(srcPages).filter((file) => path.extname(file) === '.html')
 
 // Process each HTML file
 htmlFiles.forEach((fileName) => {
-  const srcFilePath = path.join(pagesDir, fileName)
-  const distFilePath = path.join(distDir, fileName)
+  const srcFilePath = path.join(srcPages, fileName)
+  const distFilePath = path.join(dist, fileName)
   replaceTagsInFile(srcFilePath, distFilePath)
   console.log(`Replaced tags in ${fileName} and saved to dist directory.`)
 })
 
-console.log(`Tags replaced in all HTML pages and saved to the ${distDir} directory. 👍 \n`)
+console.log(`Tags replaced in all HTML pages and saved to the ${dist} directory. 👍 \n`)
 
-// /* ************************************************************************************** */
-// // Start optimization
-// console.log(`Start optimization all pages in ${distDir} 🔨 \n`)
+/* ************************************************************************************** */
+// Start optimization
+console.log(`Start optimization all pages in ${dist} 🔨 \n`)
 
-// // Optimize HTML in a given file
-// function optimizeHtmlFile(filePath) {
-//   let htmlContent = fs.readFileSync(filePath, 'utf-8')
-//   const minifiedHtml = minify(htmlContent, {
-//     collapseWhitespace: true,
-//     removeComments: true,
-//     minifyJS: true,
-//     minifyCSS: true,
-//   })
-//   fs.writeFileSync(filePath, minifiedHtml, 'utf-8')
-// }
+// Optimize HTML in a given file
+function optimizeHtmlFile(filePath) {
+  let htmlContent = fs.readFileSync(filePath, 'utf-8')
+  const minifiedHtml = minify(htmlContent, {
+    collapseWhitespace: true,
+    removeComments: true,
+    minifyJS: true,
+    minifyCSS: true,
+  })
+  fs.writeFileSync(filePath, minifiedHtml, 'utf-8')
+}
 
-// // Get a list of all HTML files in the dist directory
-// const distFiles = fs.readdirSync(distDir).filter((file) => path.extname(file) === htmlFileExtension)
+// Get a list of all HTML files in the dist directory
+const distFiles = fs.readdirSync(dist).filter((file) => path.extname(file) === '.html')
 
-// // Process each HTML file in the dist directory
-// distFiles.forEach((fileName) => {
-//   const distFilePath = path.join(distDir, fileName)
-//   optimizeHtmlFile(distFilePath)
-//   console.log(`Optimized ${fileName} in the dist directory.`)
-// })
+// Process each HTML file in the dist directory
+distFiles.forEach((fileName) => {
+  const distFilePath = path.join(dist, fileName)
+  optimizeHtmlFile(distFilePath)
+  console.log(`Optimized ${fileName} in the dist directory.`)
+})
 
-// console.log(`HTML optimization completed for all HTML files in the ${distDir} directory. 👍 \n`)
+console.log(`HTML optimization completed for all HTML files in the ${dist} directory. 👍 \n`)
 
 /* ************************************************************************************** */
 // Start optimization of images
-console.log(`Start images conversion in ${assetsDir} with sharp 🔨 \n`)
+console.log(`Start images conversion in ${srcAssetsImg} with sharp 🔨 \n`)
 
 // Create dir if it doesnt exists
-if (!fs.existsSync(distAssetsDir)) {
-  fs.mkdirSync(distAssetsDir, { recursive: true })
+if (!fs.existsSync(distAssetsImg)) {
+  fs.mkdirSync(distAssetsImg, { recursive: true })
 }
 
 // List all files in the source directory
-const files = fs.readdirSync(assetsDir)
+const files = fs.readdirSync(srcAssetsImg)
 
 // Process each image file
 files.forEach(async (file) => {
-  const sourceFilePath = path.join(assetsDir, file)
+  const sourceFilePath = path.join(srcAssetsImg, file)
 
   // Check if the file is a JPG or PNG
   if (file.endsWith('.jpg') || file.endsWith('.png')) {
-    const outputFilePath = path.join(distAssetsDir, `${file.split('.')[0]}.webp`)
+    const outputFilePath = path.join(distAssetsImg, `${file.split('.')[0]}.webp`)
 
     try {
       await sharp(sourceFilePath).toFormat('webp', { quality: 20 }).toFile(outputFilePath)
@@ -110,4 +109,30 @@ files.forEach(async (file) => {
   }
 })
 
-console.log(`Image conversion completed in ${distAssetsDir} 👍`)
+console.log(`Image conversion completed in ${distAssetsImg} 👍`)
+
+/* ************************************************************************************** */
+// Copy other assets
+console.log(`Copy other assets from ${srcAssets} to ${distAssets} dir  🔨 \n`)
+async function copyAssets(params) {
+  try {
+    await fs.copyFileSync(
+      `${srcAssets}fonts/Marck_Script/MarckScript-Regular.ttf`,
+      `${distAssets}MarckScript-Regular.ttf`
+    )
+    await fs.copyFileSync(`${srcAssets}3d/x-drive.glb`, `${distAssets}x-drive.glb`)
+    await fs.copyFileSync(
+      `${srcAssets}modules/model-viewer.min.js`,
+      `${distAssets}model-viewer.min.js`
+    )
+    await fs.copyFileSync(
+      `${srcAssets}modules/model-viewer.min.js`,
+      `${distAssets}model-viewer.min.js`
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+copyAssets()
+
+console.log(`Finish copying 👍`)
