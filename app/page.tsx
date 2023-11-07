4905682ecdc421474139eaae7452694bd1bf1c53
user <user@MacBook-Pro-user.local>
2023-11-07 17:49:52 +0200

    Add TailwindCSS with tailwind.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <h1 className=" text-3xl font-bold underline bg-slate-500 text-blue-900">Hello, Next.js!</h1>
 
       <Link href="/blog">Link to Blog</Link>
     </>
