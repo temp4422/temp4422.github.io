1a3a949afa820461ba4003138df26cdc19401e7d
user <user@MacBook-Pro-user.local>
2023-11-16 13:00:13 +0200

    WIP

--- docs/next/_next/static/chunks/app/layout-a8f506c31ea72602.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8945:function(n,e,u){Promise.resolve().then(u.t.bind(u,2853,23))},2853:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=8945)}),_N_E=n.O()}]);
