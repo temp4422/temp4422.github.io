0c3ebc1f4d0e0187233abfc0bfcef20d0398f0d8
user <user@MacBook-Pro-user.local>
2023-11-23 15:37:07 +0200

    WIP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/next/',
+  basePath: '/next',
   distDir: '/docs/next',
+  // assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    
-    './docs/next/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
