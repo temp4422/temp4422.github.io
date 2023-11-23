554a1a00c2a1fcf4d05b3b622534595b91c5f9fa
user <user@MacBook-Pro-user.local>
2023-11-23 15:05:59 +0200

    Try fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // The `content` section is where you configure the paths to all of your HTML templates, JS components, and any other files that contain Tailwind class names.
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './docs/next/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
