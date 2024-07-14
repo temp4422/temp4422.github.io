576f8d27ffb6b403ad31c88dc162eb67f12e52c9
user <user@MacBook-Pro-user.local>
2024-07-14 17:25:24 +0300

    Remove Next.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/next',
-  distDir: '/docs/next',
-  // assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
-}
-
-module.exports = nextConfig
