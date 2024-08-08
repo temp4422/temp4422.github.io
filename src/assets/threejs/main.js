1b1841eca4ac3c5359d4f69fe51d0d4ecf763d89
user <user@MacBook-Pro-user.local>
2024-08-08 21:21:50 +0300

    Add rotation to 3d object

--- src/assets/threejs/main.js
+++ src/assets/threejs/main.js
@@ -42,7 +42,7 @@ loader.load(
 
     // Animate object 60times/second
     function animate() {
-      // model.rotation.y += 0.005
+      model.rotation.y += 0.005
       renderer.render(scene, camera)
       // controls.update()
     }
