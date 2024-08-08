4ee4d98b9c143d7f33243fdaa8afaa8aba73542a
user <user@MacBook-Pro-user.local>
2024-08-08 13:44:32 +0300

    Add transparency with alpha

--- src/assets/threejs/main.js
+++ src/assets/threejs/main.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ alpha: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 const main = document.querySelector('#box')
 main.appendChild(renderer.domElement)
